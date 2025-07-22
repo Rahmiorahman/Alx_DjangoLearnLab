8b546d02c49ff257e7c426efbdc142004d3384df
abdirahman Hassan <abdirahmanhassaney@gmail.com>
2025-07-22 22:03:41 +0300

    Add bookshelf app with Book model and admin setup

--- bookshelf/admin.py
+++ bookshelf/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Book
 
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')  # Columns to display in list view
+    list_filter = ('publication_year',)  # Filter sidebar by publication year
+    search_fields = ('title', 'author')  # Enable search by title and author
 # Register your models here.
